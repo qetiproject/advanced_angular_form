91f6611392721f2d4fa19c92f09c530be41df8b5
Qeti Khetsuriani <ketideveloper@gmail.com>
2024-11-12 21:48:15 +0400

    task-form

--- projects/forms-playground/src/app/app-routing.ts
+++ projects/forms-playground/src/app/app-routing.ts
@@ -1,18 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    title: 'Template-Driven Forms Playground',
-    loadComponent:
-      () => import('./playgrounds/template-forms/template-forms-page/template-forms-page.component')
-        .then(m => m.TemplateFormsPageComponent)
-  },
-  {
-    path: 'reactive-forms',
-    title: 'Reactive Forms Playground',
-    loadComponent:
-      () => import('./playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component')
-        .then(m => m.ReactiveFormsPageComponent)
-  },
+
 ];
