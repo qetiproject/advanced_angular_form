d0547e84c1764b6c98dd035a87e2205f68dabbe7
Qeti Khetsuriani <ketideveloper@gmail.com>
2024-12-25 10:21:09 +0400

    reactive forms

--- projects/forms-playground/src/app/app-routing.ts
+++ projects/forms-playground/src/app/app-routing.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TaskFormComponent } from './playgrounds/task-form/task-form.component';
 import { WeatherWidgetComponent } from './playgrounds/weather-widget/weather-widget.component';
 import { WeatherComponent } from './playgrounds/weather/weather.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const routes: Routes = [
   
@@ -12,5 +13,12 @@ export const routes: Routes = [
   {
     path: 'weather',
     component: WeatherComponent
-  }
+  },
+  {
+    path: 'reactive-forms',
+    title: 'Reactive Forms Playground',
+    loadComponent:
+      () => import('./playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component')
+        .then(m => m.ReactiveFormsPageComponent)
+  },
 ];
