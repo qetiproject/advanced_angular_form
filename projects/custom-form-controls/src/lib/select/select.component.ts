5c92eb2d6c45fb4a99a674c72558af97ff194bb2
Qeti Khetsuriani <ketideveloper@gmail.com>
2024-12-28 09:28:11 +0400

    dropdown animation

--- projects/custom-form-controls/src/lib/select/select.component.ts
+++ projects/custom-form-controls/src/lib/select/select.component.ts
@@ -1,9 +1,19 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'cfc-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
+  animations: [
+    trigger('dropDown', [
+      state('void', style({ transform: 'scaleY(0)', opacity: 0 })),
+      state('*', style({ transform: 'scaleY(1)', opacity: 1 })),
+      transition(':enter', [animate('320ms cubic-bezier(0, 1, 0.45, 1.34)')]),
+      transition(':leave', [animate('420ms cubic-bezier(0.88,-0.7, 0.86, 0.85)')]),
+    ])
+  ]
 })
 export class SelectComponent implements OnInit {
 
@@ -18,6 +28,12 @@ export class SelectComponent implements OnInit {
     this.isOpen = true;
   }
   
+  @Output()
+  readonly opened = new EventEmitter<void>();
+
+  @Output()
+  readonly closed = new EventEmitter<void>();
+  
   isOpen: boolean = false;
 
   constructor() { }
@@ -25,4 +41,17 @@ export class SelectComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onPanelAnimationDone({ fromState, toState }: any) {
+    if (fromState === 'void' && toState === null && this.isOpen) {
+      this.opened.emit();
+    }
+    if (fromState === null && toState === 'void' && !this.isOpen) {
+      this.closed.emit();
+    }
+  }
+
+  close(){
+    this.isOpen = false;
+  }
+  
 }
--- projects/custom-form-controls/src/public-api.ts
+++ projects/custom-form-controls/src/public-api.ts
@@ -3,4 +3,4 @@
  */
 export * from './lib/rating-picker/rating-picker.component';
 export { SelectModule} from './lib/select/select.module';
-import { SelectComponent } from './lib/select/select.component';
+export { SelectComponent } from './lib/select/select.component';
